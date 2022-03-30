ee4ab946805ee0e43da66ed03063be532839fe34
Jeffrey Cook <cookturingedu@gmail.com>
2022-03-29 22:46:37 -0400

    Add number variables and comments

--- variables.js
+++ variables.js
@@ -1,3 +1,9 @@
+//Add a few variables that are assigned to Strings
 var myName = "Jeff";
 var favFood = "Wings"
 var school = "Turing"
+
+//Add a few variables that are assigned to Numbers
+var year = 2022;
+var monthsOfSchool = 7;
+var modOneStartMonth = 5;
